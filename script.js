87cb2ab662f7347c8de9cf1fdfe415f0fd250804
Glitch (glitch-hello-website) <none>
2023-12-14 17:31:03 +0000

    🐆🍥 Checkpoint ./style.css:62997810/4408 ./index.html:62997810/2742 ./script.js:62997810/1723

--- script.js
+++ script.js
@@ -1,34 +1,18 @@
-/*
-  This is your site JavaScript code - you can add interactivity!
-*/
+window.addEventListener("load",AddListeners)
 
-// Print a message in the browser's dev tools console each time the page loads
-// Use your menus or right-click / control-click and choose "Inspect" > "Console"
-console.log("Hello 🌎");
-
-/* 
-Make the "Click me!" button move when the visitor clicks it:
-- First add the button to the page by following the steps in the TODO 🚧
-*/
-const btn = document.querySelector("button"); // Get the button from the page
-if (btn) { // Detect clicks on the button
-  btn.onclick = function () {
-    // The 'dipped' class in style.css changes the appearance on click
-    btn.classList.toggle("dipped");
-  };
+function AddListeners(){
+  document.getElementById("btnSubmit").addEventListener("click",Multiple)
 }
 
-
-// ----- GLITCH STARTER PROJECT HELPER CODE -----
-
-// Open file when the link in the preview is clicked
-let goto = (file, line) => {
-  window.parent.postMessage(
-    { type: "glitch/go-to-line", payload: { filePath: file, line: line } }, "*"
-  );
-};
-// Get the file opening button from its class name
-const filer = document.querySelectorAll(".fileopener");
-filer.forEach((f) => {
-  f.onclick = () => { goto(f.dataset.file, f.dataset.line); };
-});
+function Multiple(){
+  var n = 0
+  var input = document.getElementById("txtInput").value
+  var ans = ""
+  input = parseInt(input)
+  while (n < 5){
+    input = input * 2
+    n = n + 1
+    
+  }
+  
+}
