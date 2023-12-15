b9e46a7244258e787a6decfb02a669d7dcfb2d7e
Glitch (glitch-hello-website) <none>
2023-12-15 17:16:19 +0000

    👑📼 Checkpoint ./script.js:62997810/936 ./notes.txt:62997810/159

--- script.js
+++ script.js
@@ -1,17 +1,25 @@
 window.addEventListener("load",AddListeners)
 
 function AddListeners(){
-  document.getElementById("btnSubmit").addEventListener("click",Multiple)
+  document.getElementById("btnSubmit").addEventListener("click",Check)
+}
+
+function Check(){
+  var input = document.getElementById("txtInput").value
+  while (input < 6 || input > 20){
+    alert("Error enter a number from 6-20.")
+  }
+  Multiple()
 }
 
 function Multiple(){
   var n = 0
   var input = document.getElementById("txtInput").value
-  var ans = ""
+  var ans = String(input)
   input = parseInt(input)
-  while (n < 5){
+  while (n < 4){
     input = input * 2
-    ans = ans + String(input)+ "," + " ";
+    ans = ans + "," + " " +String(input);
     n = n + 1
   }
   document.getElementById("lblresult").innerText = ans
